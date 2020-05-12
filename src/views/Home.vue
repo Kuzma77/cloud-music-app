64b4f17593e3ab0fa096856211c12ff449f8fc40
Kuzma77 <2673327266@qq.com>
2020-05-12 20:07:30 +0800

    结合vant库进行云音乐app简单搭建和库的学习使用

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <van-nav-bar border fixed @click-left="onClickLeft" @click-right="onClickRight">
+      <template #left>
+        <van-icon name="contact" size="18" />
+      </template>
+      <template #right>
+        <van-icon name="search" size="18" />
+      </template>
+      <template #title>
+        <van-row gutter="20" type="flex" justify="center">
+          <van-col span="6">我的</van-col>
+          <van-col span="6">发现</van-col>
+          <van-col span="6">云村</van-col>
+          <van-col span="6">视频</van-col>
+        </van-row>
+      </template>
+    </van-nav-bar>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { Toast } from 'vant'
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+  name: 'Home',
+  data() {
+    return {}
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {
+    onClickLeft() {
+      Toast('返回')
+    },
+    onClickRight() {
+      Toast('按钮')
+    }
+  },
+  computed: {}
+}
 </script>
+
+<style scoped lang="scss"></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <van-row type="flex" justify="center" align="center">
+      <van-col span="18">
+        <van-form @submit="onSubmit">
+          <van-field
+            v-model="username"
+            name="用户名"
+            label="用户名"
+            placeholder="用户名"
+            :rules="[{ required: true, message: '请填写用户名' }]"
+          />
+          <van-field
+            v-model="password"
+            type="password"
+            name="密码"
+            label="密码"
+            placeholder="密码"
+            :rules="[{ required: true, message: '请填写密码' }]"
+          />
+          <div style="margin: 16px;">
+            <van-button round block color="linear-gradient(to right, #4bb0ff, #6149f6)" size="small" native-type="submit" to="/">
+              提交
+            </van-button>
+          </div>
+        </van-form>
+      </van-col>
+    </van-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values)
+    }
+  },
+  computed: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
